e4cf56278aa5a6e2d93df34162963888dd62e5ee
AkramAbuOwaimer <Akram.AbuOwaimer@gmail.com>
2020-09-01 08:02:51 +0300

    Real Initial Commit ^_^

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["numpy==1.19.1"]
+
+setup(
+    name="pidcon",
+    version="0.0.1",
+    author="Akram Abu Owaimer",
+    author_email="Akram.AbuOwaimer@gmail.com",
+    description="A package to handle simple PID controller",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FutureTech-o/pidcon",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
